65d6ad6fb37e6e100415c022e3fe317233d81b5f
M.B <43449726+bainscoder@users.noreply.github.com>
2026-09-01 17:57:55 +0530

    removing only

--- src/tests/bookStoreApplication/signup.spec.js
+++ src/tests/bookStoreApplication/signup.spec.js
@@ -2,7 +2,7 @@ import test from '../../support/fixture/fixture';
 
 test.describe('Sign up page tests', () => {
 
-    test.only('should navigate to sign up page', async ({ signUp }) => {
+    test('should navigate to sign up page', async ({ signUp }) => {
         await signUp.navigateToSignUpPage();
     });
 });
